d2bbbfa356b0cf5bfd23da3200e87ca72eaa803b
cewilliams <cewilliams@csbsju.edu>
2018-08-01 21:42:53 -0500

    logic

--- FriendFinder/server.js
+++ FriendFinder/server.js
@@ -2,3 +2,22 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
 
+var app = express()
+
+var port = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded(
+    {
+        extended: true
+    }
+))
+
+app.use(bodyParser.json())
+
+require("./app/routing/apiRoutes.js")(app)
+require("./app/routing/htmlRoutes.js")(app)
+
+
+app.listen(port, function(){
+    console.log("app is listening on port: " + port)
+})
